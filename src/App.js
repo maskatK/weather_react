e88af69c7df649295ca9ed6111dc818a24eb2947
maskatK <69219044+maskatK@users.noreply.github.com>
2022-09-22 21:34:49 +0200

    Delyte tempeature

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React from "react";
 import "./Add.css";
 import Search from "./Search";
 
-import Temperature from "./Temperature";
-
 export default function App() {
   return (
     <div className="cont_weather_and_search">
